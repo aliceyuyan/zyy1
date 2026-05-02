179ef1fa64f14756345d7ff5d7a7c7e409f35027
aliceyuyan <155928771+aliceyuyan@users.noreply.github.com>
2026-05-02 20:42:44 +0800

    Add files via upload

---
+++ test_single_end/test_single.sh
@@ -0,0 +1,4 @@
+snippy --outdir 'ERR1596541' --se '/home/alice/data/anthracis/test_single_end/trimmomatic/ERR1596541_single.fastq.gz' --ref /home/alice/data/anthracis/NC_007530.2.fasta --cpus 20
+snippy --outdir 'ERR1596542' --se '/home/alice/data/anthracis/test_single_end/trimmomatic/ERR1596542_single.fastq.gz' --ref /home/alice/data/anthracis/NC_007530.2.fasta --cpus 20
+snippy --outdir 'ERR1596543' --se '/home/alice/data/anthracis/test_single_end/trimmomatic/ERR1596543_single.fastq.gz' --ref /home/alice/data/anthracis/NC_007530.2.fasta --cpus 20
+snippy-core --ref 'ERR1596541/ref.fa' ERR1596541 ERR1596542 ERR1596543
